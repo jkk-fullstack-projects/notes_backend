b5cee4a7c2825b2c44b0f6a916c6cdae3ecc8da8
JouniGitti <kalaksi@gmail.com>
2024-03-09 13:45:03 +0200

    Corrected typos regarding Note object, created new post Request

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static('dist'));
 
-const note = require('./models/note.js');
+const Note = require('./models/note.js');
 
 let notes = [
   {
@@ -33,15 +33,19 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
+  Note.findById(request.params.id)
+    .then(note => {
+      response.json(note)
+    })  
+  /*const id = Number(request.params.id)
     const note = notes.find(note => note.id === id)
     
     if (note) {
       response.json(note)
     } else {
       response.status(404).end()
-    }
-})
+    }*/
+});
 
 app.get('/api/notes', (request, response) => {
   note.find({})
@@ -61,21 +65,21 @@ const generateId = () => {
 app.post('/api/notes', (request, response) => {
   const body = request.body
   
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing'
-    })
+  if (body.content === undefined) {
+    return response.status(400)
+      .json({error: 'content missing'})
   };
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  };
+  });
 
-  notes = notes.concat(note);
-
-  response.json(note);
+  //notes = notes.concat(note);
+  note.save()
+    .then(savedNote => {
+      response.json(savedNote);
+    })
 });
 
 app.delete('/api/notes/:id', (request, response) => {
