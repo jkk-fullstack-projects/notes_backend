0d68fbebe722b42fdf637d0e5f9f437c9922534e
JouniGitti <kalaksi@gmail.com>
2024-03-10 08:30:42 +0200

    Created a mongoose validator for a note length (min 5) and note required and streamlining code.

--- index.js
+++ index.js
@@ -32,19 +32,16 @@ app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id)
     .then(note => {
-      response.json(note)
-    })  
-  /*const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-    
-    if (note) {
-      response.json(note)
-    } else {
-      response.status(404).end()
-    }*/
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch(error => next(error))
 });
 
 app.get('/api/notes', (request, response) => {
@@ -75,19 +72,52 @@ app.post('/api/notes', (request, response) => {
     important: body.important || false,
   });
 
-  //notes = notes.concat(note);
   note.save()
     .then(savedNote => {
       response.json(savedNote);
     })
 });
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body;
 
-    response.status(204).end()
-})
+  const note = {
+    content: body.content,
+    important: body.important,
+  };
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  .then(updatedNote => {
+    response.json(updatedNote);
+  })
+  .catch(error => next(error));
+});
+
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400)
+    .send({ error: 'malformatted id' });
+  }
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
